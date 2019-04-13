16c240d07612035836b9336b6e421ab30bd4ed41
JesusKim <jesuskim666@gmail.com>
2019-04-14 03:30:08 +0900

    Update main.cpp

--- JWGame/main.cpp
+++ JWGame/main.cpp
@@ -19,7 +19,7 @@ int main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	// TODO:
-	// Picking the bounding sphere!
+	// Picking the (multiple) bounding sphere!
 	// Collision!
 	// Instancing!
 
