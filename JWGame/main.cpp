d8c42ec044d69ab4589298f3cbe161ac997c8943
JesusKim <jesuskim666@gmail.com>
2019-05-02 02:21:22 +0900

    Update main.cpp

--- JWGame/main.cpp
+++ JWGame/main.cpp
@@ -19,7 +19,7 @@ int main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	// TODO:
-	// # ??			@ Height-map based terrain
+	// # ??			@ Height-map based terrain => quad-tree => frutum culling
 	// # Physics	@ Collision
 	// # Physics	@ Light/Camera representations must be pickable but not subject to physics, so these must be NonPhysical type
 	// # Render		@ Sprite instancing
