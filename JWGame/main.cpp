c0b7b0f94d9c9021e494c5a8fb9c3a05b129f061
JesusKim <jesuskim666@gmail.com>
2019-05-06 01:55:36 +0900

    Update main.cpp

--- JWGame/main.cpp
+++ JWGame/main.cpp
@@ -19,6 +19,7 @@ int main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	// TODO:
+	// # Terrain	@ AABB Picking is better for tree structured terrain... (Regions overlap when using bounding spheres)
 	// # Physics	@ Collision
 	// # Physics	@ Light/Camera representations must be pickable but not subject to physics, so these must be NonPhysical type
 	// # Render		@ Sprite instancing
